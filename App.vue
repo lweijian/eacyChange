18a4d33bf6d021778804619c2cb974fa67ebfc7c
m0_46688383 <760560917@qq.com>
2021-09-28 18:56:33 +0800

    订单基础模块
    Signed-off-by: m0_46688383 <760560917@qq.com>

--- App.vue
+++ App.vue
@@ -1,58 +1,36 @@
 <script>
-	
 	import config from '@/config.js'
 
 	export default {
-<<<<<<< .mine
-		onPageNotFound(msg) {
-			uni.redirectTo({
-		    	url: config.error.url
-			})
 
-=======
 		onPageNotFound(msg) {
 			uni.redirectTo({
 				url: config.error.url
 			})
 		},
->>>>>>> .theirs
-		onLaunch: function () {
-<<<<<<< .mine
 
-=======
+		onLaunch: function() {
 			console.log('App Launch')
->>>>>>> .theirs
 		},
-			onLaunch: function () {
-			console.log('App Launch') 
-		},
-		onShow: function () {
+		onShow: function() {
 			console.log('App Show')
 		},
-		onHide: function () {
+		onHide: function() {
 			console.log('App Hide')
 		}
 	}
 </script>
 
-<style lang="scss">
-	/*每个页面公共css */
-	@import "uview-ui/index.scss";
-	@import "/static/iconfont/font.scss";
-	/* #ifdef H5 */ uni-page-head { display: none; } /* #endif */
-</style>
-		onShow: function () {
-			console.log('App Show')
-		},
-		onHide: function () {
-			console.log('App Hide')
-		}
-	}
-</script>
 
 <style lang="scss">
 	/*每个页面公共css */
 	@import "uview-ui/index.scss";
 	@import "/static/iconfont/font.scss";
-	/* #ifdef H5 */ uni-page-head { display: none; } /* #endif */
+
+	/* #ifdef H5 */
+	uni-page-head {
+		display: none;
+	}
+
+	/* #endif */
 </style>
